19bc9f42e51d8031cb41c4a7c519b59ae2eb0508
mrestuab <mrestuab54@gmail.com>
2026-08-14 00:06:53 +0700

    link deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backendmedislink-production.up.railway.app/api',
+  baseURL: 'https://backend-medislink.vercel.app/api',
 });
 
 api.interceptors.request.use(
